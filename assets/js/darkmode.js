3ac7f2ffad9a6df2f119a22003fa76bad4151548
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-05-01 14:30:30 -0300

    feat: Add Dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,9 +1,26 @@
 const body = document.body;
+const darkMode = document.getElementById("darkMode");
+
+if (body.classList.contains("dark")) {
+	darkMode.checked = true;
+}
 
 document.addEventListener("keydown", function (event) {
 	if (event.key === "b" || event.key === "B") {
 		console.log("Tecla B pressionada!");
 
+		body.classList.toggle("dark");
+	}
+
+	if (body.classList.contains("dark")) {
+		darkMode.checked = true;
+	} else {
+		darkMode.checked = false;
+	}
+});
+
+darkMode.addEventListener("click", () => {
+	if (darkMode.value) {
 		body.classList.toggle("dark");
 	}
 });
